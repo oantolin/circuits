72353b963ecf0a80f8ca076c7e0677078d7b7d3a
Omar Antolín <omar.antolin@gmail.com>
2021-11-07 15:11:58 -0600

    New implementation
    There are two main differences:
    
    - Arguments to operators are now functions rather than expressions, to
    reduce duplication. The result is that finding all expressions now is
    about as fast as finding a single expression used to be.
    
    - I've merged the minexprs and mincircuits functions, since the
    minexprs code was overly general.

--- circuits.lisp
+++ circuits.lisp
@@ -1,14 +1,17 @@
-(defun partitions (n k m)
-  "Return all partitions of N into K parts, each at most m.
+(defmacro dorange ((var from to) &body body)
+  `(loop for ,var from ,from to ,to do ,@body))
+
+(defun partitions (n k l m)
+  "Return all partitions of N into between K and L parts, each at most M.
 Each partition is represented as an alist of parts and multiplicities.
 For example ((3 . 2) (2 . 1)) represents the partition 3+3+2 of 8."
   (cond
     ((= n k 0) '(()))
-    ((or (= n 0) (= k 0) (= m 0)) nil)
-    (t (let ((results (partitions n k (1- m))))
-         (dotimes (i (min k (floor n m)))
-           (dolist (p (partitions (- n (* (1+ i) m)) (- k i 1) (1- m)))
-             (push (cons (cons m (1+ i)) p) results)))
+    ((or (= n 0) (= l 0) (= m 0)) nil)
+    (t (let ((results (partitions n k l (1- m))))
+         (dorange (i 1 (min l (floor n m)))
+           (dolist (p (partitions (- n (* i m)) (max 0 (- k i)) (- l i) (1- m)))
+             (push (cons (cons m i) p) results)))
          results))))
 
 (defun subsets (k list)
@@ -17,10 +20,12 @@ The order of the elements in each subset is the same as in LIST."
   (cond
     ((= k 0) '(()))
     ((= k 1) (mapcar #'list list))
+    ((= k 2) (loop for (x . rest) on list
+                   nconc (loop for y in rest collect (list x y))))
     ((null list) nil)
     (t (destructuring-bind (x . rest) list
-         (append (mapcar (lambda (s) (cons x s)) (subsets (1- k) rest))
-                 (subsets k rest))))))
+         (nconc (mapcar (lambda (s) (cons x s)) (subsets (1- k) rest))
+                (subsets k rest))))))
 
 (defun mapcart (fn lists)
   "Apply FN to all elements of the cartesion product of LISTS."
@@ -31,91 +36,78 @@ The order of the elements in each subset is the same as in LIST."
                        (cdr lists)))
             (car lists))))
 
-(defun minexprs (ops atoms &key goal complexity)
-  "Find all minimal expressions for given values in terms of OPS and ATOMS.
-
-OPS should be a list of 4-element lists of the following form:
-(name func min-arity max-arity). The max-arity can be the symbol
-:infinity. The operations are assumed to be commutative and
-idempotent, and thus to reduce repetitions of the expression an
-arbitrary total order is imposed on the expressions and operations are
-only constructed with arguments in increasing order.
-
-ATOMS should be a list of 2-element lists of the form (name value).
-
-The COMPLEXITY keyword parameter is the maximum expression complexity
-to explore. The complexity of an expression is the total number of
-operations and atoms that it comprises.
-
-The GOAL parameter is a target number of values for which to find a
-minimal expression. If the goal is met, computation stops even before
-the maximum COMPLEXITY is reached.
-
-At least one of GOAL and COMPLEXITY should be given.
-
-The return value is a hash-table mapping values to a list whose first
-element is the minimal complexity of expression that produces the
-value, and whose tail is the list of all expressions of that minimum
-complexity."
-  (let ((values (make-array 2 :initial-element nil
-                              :fill-pointer 2 :adjustable t))
-        ;; values[i] holds an alist of (value . expr) pairs of complexity i
-        (found 0)
-        (minexpr (make-hash-table)))
-    (dolist (nv atoms) ; nv is a (name value) list
-      (incf found)
-      (push (cons (cadr nv) (car nv)) (aref values 1))
-      (setf (gethash (cadr nv) minexpr) (list 1 (car nv))))
-    (do ((cx 2 (1+ cx)))
-        ((or (and goal (>= found goal))
-             (and complexity (> cx complexity)))
-         minexpr)
-      (vector-push-extend nil values)
-      (dolist (op ops)
-        (destructuring-bind (name func min-arity max-arity) op
-          (dotimes (i (1+ (- (if (eq max-arity :infinity) (1- cx) max-arity)
-                             min-arity)))
-            (let ((arity (+ i min-arity)))
-              (dolist (part (partitions (1- cx) arity (1- cx)))
-                (mapcart (lambda (args)
-                           (setf args (reduce #'append args :from-end t))
-                           (let* ((val (apply func (mapcar #'car args)))
-                                  (seen (gethash val minexpr)))
-                             (when (or (not seen) (= (car seen) cx))
-                               (let ((expr (cons name (mapcar #'cdr args))))
-                                 (unless seen
-                                   (incf found)
-                                   (push cx (gethash val minexpr)))
-                                 (push expr (cdr (gethash val minexpr)))
-                                 (push (cons val expr) (aref values cx))))))
-                         (mapcar (lambda (p)
-                                   (subsets (cdr p) (aref values (car p))))
-                                 part))))))))))
-
 (defun mincircuits (n &key complexity max-arity)
-  "Find minimal circuits for all N variable boolean functions.
+  "Find minimal circuits for all boolean functions of N variables.
 
-The optional COMPLEXITY argument specifies a maximum complexity for
-the search.
+The circuits use NOT, AND & OR gates. The AND & OR gates used are
+allowed to have arity between 2 and MAX-ARITY, which, if omitted
+defaults to N. One can also use the symbol :infinity, to specify there
+is no upper bound on arity.
 
-The optional MAX-ARITY argument specifies a maximum arity for the AND
-and OR gates. If absent it defaults to N. You can also use the symbol
-:infinity."
+The return value is an array of length 2^2^n associating to each
+boolean function a list of minimal circuits for it. The circuits are
+specified by a list whose first element is the type of gate, and whose
+remaining elements are other boolean functions.
+
+The optional argument COMPLEXITY specifies a maximum complexity to
+bound the search."
   (unless max-arity (setf max-arity n))
   (flet ((pp (k) (ash 1 (ash 1 k))))
     (let* ((mask (1- (pp n)))
-           (variable-names
-             (subseq '(p q r s t u v w x y z) 0 n)) ; even n=5 is too high :)
-           (projections
-             (loop for k below n
-                   collect (/ (* (1- (pp n)) (pp k)) (1+ (pp k))))))
-      (minexprs
-       `((and ,#'logand 2 ,max-arity)
-         (or ,#'logior 2 ,max-arity)
-         (not ,(lambda (x) (logand mask (lognot x))) 1 1))
-       (mapcar #'list variable-names projections)
-       :complexity complexity
-       :goal (ash 1 (ash 1 n))))))
+           (atoms
+             (append
+              `((0 . 0) (1 . ,mask))
+              (loop for var in '(p q r s t u v w x y z) and k below n
+                    collect (cons var (/ (* (1- (pp n)) (pp k)) (1+ (pp k)))))))
+           (total 0)
+           (minexpr (make-array (pp n) :initial-element nil))
+           (fns-w/cx (make-array 2 :initial-element nil
+                                   :fill-pointer 2 :adjustable t)))
+      (flet ((found (cx fn expr) 
+               (let ((seen (aref minexpr fn)))
+                 (when (or (not seen) (= cx (car seen)))
+                   (unless seen
+                     (incf total)
+                     (push cx (aref minexpr fn))
+                     (push fn (aref fns-w/cx cx)))
+                   (push expr (cdr (aref minexpr fn)))))))
+        ;; complexity 1: constants 0, 1 & variables
+        (dolist (name-func atoms)
+          (destructuring-bind (name . func) name-func
+            (found 1 func name)))
+        ;; higher complexities
+        (do ((cx 2 (1+ cx)))
+            ((or (= total (pp n)) (and complexity (> cx complexity)))
+             minexpr)
+          (vector-push-extend nil fns-w/cx)
+          ;; NOT gates
+          (dolist (fn (aref fns-w/cx (1- cx)))
+            (found cx (logand mask (lognot fn)) (list 'not fn)))
+          ;; AND & OR gates
+          (let ((max-parts (if (eq max-arity :infinity) (1- cx) max-arity)))
+            (dolist (part (partitions (1- cx) 2 max-parts (1- cx)))
+              (mapcart
+               (lambda (args)
+                 (setf args (reduce #'append args :from-end t))
+                 (found cx (apply #'logior args) (cons 'or args))
+                 (found cx (apply #'logand args) (cons 'and args)))
+               (mapcar (lambda (p) (subsets (cdr p) (aref fns-w/cx (car p))))
+                       part)))))))))
+
+(defun expand-circuits (fn table)
+  "Return all circuits for FN implicitly in TABLE."
+  (labels ((rec (fn)
+             (mapcan (lambda (circuit)
+                       (if (atom circuit)
+                           (list circuit)
+                           (let (all)
+                             (mapcart
+                              (lambda (args)
+                                (push (cons (car circuit) args) all))
+                              (mapcar #'rec (cdr circuit)))
+                             (nreverse all))))
+                     (cdr (aref table fn)))))
+    (rec fn)))
 
 (defun save-mincircuits (n &key (file "mincircuits~d.txt")
                              complexity max-arity verbose)
@@ -136,12 +128,12 @@ no circuit was found. By default, these functions are silently omitted."
     (let ((circuits (mincircuits n :complexity complexity
                                    :max-arity max-arity)))
       (dotimes (k (ash 1 (ash 1 n)))
-        (let ((me (gethash k circuits)))
-          (if (null me)
+        (let ((cx (car (aref circuits k))))
+          (if (null cx)
               (when verbose
                 (format out "~%~v,'0b no circuit found" (ash 1 n) k))
               (format out "~%~v,'0b [~2d] ~a"
-                      (ash 1 n) k (car me) (cdr me))))))))
+                      (ash 1 n) k cx (expand-circuits k circuits))))))))
 
 ;;; auxiliary functions for collecting statistics on circuits
 
